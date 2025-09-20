6457ce316423a2834244877f7b1320f078584e6e
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-09-21 00:25:14 +0530

    Update embedding.py

--- research_agent/embedding.py
+++ research_agent/embedding.py
@@ -1,4 +1,3 @@
-# research_agent/embedding.py
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
@@ -10,3 +9,4 @@ class LocalEmbeddingEngine:
         # Returns a numpy array
         emb = self.model.encode([text], convert_to_numpy=True)
         return emb[0]
+
